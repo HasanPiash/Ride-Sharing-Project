2a6f2a6e13a84b656a694366db2672ce03a00d60
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2025-10-04 00:17:38 +0600

    Update vehicle.py

--- vehicle.py
+++ vehicle.py
@@ -5,10 +5,12 @@ class Vehicle(ABC):
         'bike':60,
         'cng':15
     }
+    
     def __init__(self,vehicle_type, license_plate, rate)->None:
         self.vehicle_type=vehicle_type
         self.license_plate=license_plate
         self.rate=rate
+        
 class Car(Vehicle):
     def __init__(self, vehicle_type, license_plate, rate)->None:
         super().__init__(vehicle_type, license_plate, rate)
